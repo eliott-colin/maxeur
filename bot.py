2e546b0df4fabea6d0ecd4ded1f870ad2ccbf945
elicoptere <161453411+eliott-colin@users.noreply.github.com>
2025-08-18 01:42:45 +0200

    feat: add / commands

--- bot.py
+++ bot.py
@@ -1,23 +1,17 @@
 import discord
-from discord import app_commands
 from discord.ext import commands
 import os
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=None, intents=intents) 
 
-# Synchronisation des slash commands
 @bot.event
 async def on_ready():
-    await bot.tree.sync()  # synchronise les slash commands avec Discord
+    await bot.tree.sync()
     print(f"✅ Connecté en tant que {bot.user}")
 
-# Commande slash /ping
 @bot.tree.command(name="ping", description="Répond Pong ! 🏓")
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message("Pong ! 🏓")
 
-# Récupère le token depuis la variable d'environnement
-TOKEN = os.getenv("TOKEN")
-
-bot.run(TOKEN)
+bot.run(os.getenv("TOKEN"))
